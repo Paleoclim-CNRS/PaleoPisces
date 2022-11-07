4c403d15b1eb44facecbe5ea858ceb33a2223466
Anthogr <87010226+Anthogr@users.noreply.github.com>
2022-11-07 11:02:45 +0100

    update program

--- src/USER_INPUT/config_card.py
+++ src/USER_INPUT/config_card.py
@@ -1,4 +1,10 @@
-"""config.card settings"""
+"""
+config.card settings
+
+Note:
+    JOB_NAME can only contain alphanumeric characters, "." and "-"
+    JOB_NAME must start with a letter
+"""
 
 JOB_NAME = ''
 DATE_BEGIN = ''
